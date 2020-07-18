42e6cb10e286e1ff1211e8d893fb92b9ee03f687
handongqignxue <1417140290@qq.com>
2020-07-18 15:30:52 +0800

    首次提交

---
+++ app/src/main/java/com/hualing/znczscanapp/activities/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hualing.znczscanapp.activities;
+
+import android.os.Bundle;
+
+import com.hualing.znczscanapp.activities.BaseActivity;
+import com.hualing.znczscanapp.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initLogic() {
+
+    }
+
+    @Override
+    protected void getDataFormWeb() {
+
+    }
+
+    @Override
+    protected void debugShow() {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_main;
+    }
+}
