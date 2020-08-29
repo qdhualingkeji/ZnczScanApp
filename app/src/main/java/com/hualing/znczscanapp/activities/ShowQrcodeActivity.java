df10ba21aef8341ad91a3b3accc385919176b852
handongqignxue <1417140290@qq.com>
2020-08-29 15:37:08 +0800

    完善

--- app/src/main/java/com/hualing/znczscanapp/activities/ShowQrcodeActivity.java
+++ app/src/main/java/com/hualing/znczscanapp/activities/ShowQrcodeActivity.java
@@ -30,8 +30,8 @@ import butterknife.BindView;
 
 public class ShowQrcodeActivity extends BaseActivity {
 
-    private String dqphCode,ssddCode,ssddGroupId;
-    private JSONObject dqphGroupsIdJO,dqphGroupsFieldIdJO,ssddJbxxFieldIdJO,ziDuanNameJO;
+    private String wdddCode;
+    private JSONObject wdddJbxxFieldIdJO,wdddCriteriasIdJO,ziDuanNameJO;
     @BindView(R.id.qrcode_iv)
     ImageView qrcodeIV;
     @BindView(R.id.ddh_tv)
@@ -71,26 +71,71 @@ public class ShowQrcodeActivity extends BaseActivity {
      */
     private void initZiDuanNameJO() throws JSONException {
         ziDuanNameJO=new JSONObject();
-        ziDuanNameJO.put("二维码字段","二维码");
-        ziDuanNameJO.put("新字段组字段","新字段组");
-        ziDuanNameJO.put("所属订单字段","所属订单");
+        ziDuanNameJO.put("执行状态字段","执行状态");
         ziDuanNameJO.put("基本信息字段","基本信息");
         ziDuanNameJO.put("订单号字段","订单号");
         ziDuanNameJO.put("预装卸重量字段","预装卸重量");
         ziDuanNameJO.put("流向类型字段","流向类型");
         ziDuanNameJO.put("编辑时间字段","编辑时间");
+        ziDuanNameJO.put("二维码字段","二维码");
         ziDuanNameJO.put("实际重量字段","实际重量");
         ziDuanNameJO.put("重量差额比字段","重量差额比");
     }
 
     @Override
     protected void getDataFormWeb() {
-        initQueryKey();
+        initWDDDLtmplAttr();
     }
 
-    private void initQueryKey(){
+    private void initWDDDLtmplAttr() {
         RequestParams params = AsynClient.getRequestParams();
-        AsynClient.get(MyHttpConfing.dqphEntityListTmpl, this, params, new GsonHttpResponseHandler() {
+        AsynClient.get(MyHttpConfing.wdddEntityListTmpl, this, params, new GsonHttpResponseHandler() {
+            @Override
+            protected Object parseResponse(String rawJsonData) throws Throwable {
+                return null;
+            }
+
+            @Override
+            public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
+                Log.e("initWDDDLFail======",""+rawJsonData+","+errorResponse);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
+                Log.e("initWDDDLSuccess======",""+rawJsonResponse);
+                try {
+                    JSONObject jo=new JSONObject(rawJsonResponse);
+                    JSONObject ltmplJO = jo.getJSONObject("ltmpl");
+                    JSONArray criteriasJA = ltmplJO.getJSONArray("criterias");
+
+                    JSONArray columnsJA = ltmplJO.getJSONArray("columns");
+
+                    initWDDDCriteriasId(criteriasJA);
+                    initWDDDQueryKey();
+                } catch (JSONException e) {
+                    Log.e("error===",e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void initWDDDCriteriasId(JSONArray criteriasJA) throws JSONException {
+        wdddCriteriasIdJO=new JSONObject();
+        for(int i=0;i<criteriasJA.length();i++){
+            JSONObject criteriaJO = (JSONObject)criteriasJA.get(i);
+            String title = criteriaJO.getString("title");
+            String id = criteriaJO.getString("id");
+            Log.e("title1===",title);
+            Log.e("id1===",id);
+            wdddCriteriasIdJO.put(title,id);
+        }
+    }
+
+    private void initWDDDQueryKey() throws JSONException {
+        RequestParams params = AsynClient.getRequestParams();
+        String paramsStr="?criteria_"+wdddCriteriasIdJO.getString(ziDuanNameJO.getString("执行状态字段"))+"=待确认,排队中,待化验,待一检上磅,一检下磅,待入库,入库完成,待二检上磅,待离厂,一检称重中,二检称重中,编辑中,运输中";
+        AsynClient.get(MyHttpConfing.wdddEntityListTmpl+paramsStr, this, params, new GsonHttpResponseHandler() {
             @Override
             protected Object parseResponse(String rawJsonData) throws Throwable {
                 return null;
@@ -108,7 +153,7 @@ public class ShowQrcodeActivity extends BaseActivity {
                 try {
                     JSONObject jo = new JSONObject(rawJsonResponse);
                     String queryKey = jo.getString("queryKey");
-                    initDQPHCode(queryKey);
+                    initWDDDCode(queryKey);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -116,7 +161,7 @@ public class ShowQrcodeActivity extends BaseActivity {
         });
     }
 
-    private void initDQPHCode(String queryKey){
+    private void initWDDDCode(String queryKey){
         RequestParams params = AsynClient.getRequestParams();
         params.put("pageNo","1");
         AsynClient.get(MyHttpConfing.getEntityListData.replaceAll("queryKey",queryKey), this, params, new GsonHttpResponseHandler() {
@@ -127,20 +172,20 @@ public class ShowQrcodeActivity extends BaseActivity {
 
             @Override
             public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
-                Log.e("dQPHCodeFail======",""+rawJsonData+","+errorResponse);
+                Log.e("wDDDCodeFail======",""+rawJsonData+","+errorResponse);
             }
 
             @Override
             public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
-                Log.e("dQPHCodeSuccess======",""+rawJsonResponse);
+                Log.e("wDDDCodeSuccess======",""+rawJsonResponse);
                 try {
                     JSONObject jo = new JSONObject(rawJsonResponse);
                     JSONArray entitiesJA = jo.getJSONArray("entities");
                     JSONObject entityJO = (JSONObject)entitiesJA.get(0);
-                    dqphCode = entityJO.getString("code");
-                    Log.e("dqphCode===",""+dqphCode);
+                    wdddCode = entityJO.getString("code");
+                    Log.e("wdddCode===",""+wdddCode);
 
-                    initDQPHGroupsFieldId();
+                    initWDDDGroupsId();
                 } catch (JSONException e) {
                     Log.e("?????",e.getMessage());
                     e.printStackTrace();
@@ -149,9 +194,9 @@ public class ShowQrcodeActivity extends BaseActivity {
         });
     }
 
-    private void initDQPHGroupsFieldId(){
+    private void initWDDDGroupsId(){
         RequestParams params = AsynClient.getRequestParams();
-        AsynClient.get(MyHttpConfing.dqphDtmplNormal, this, params, new GsonHttpResponseHandler() {
+        AsynClient.get(MyHttpConfing.wdddDtmplNormal, this, params, new GsonHttpResponseHandler() {
             @Override
             protected Object parseResponse(String rawJsonData) throws Throwable {
                 return null;
@@ -159,12 +204,12 @@ public class ShowQrcodeActivity extends BaseActivity {
 
             @Override
             public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
-                Log.e("dqphFail======",""+rawJsonData+","+errorResponse);
+                Log.e("wdddFail======",""+rawJsonData+","+errorResponse);
             }
 
             @Override
             public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
-                Log.e("dqphSuccess======",""+rawJsonResponse);
+                Log.e("wdddSuccess======",""+rawJsonResponse);
 
                 try {
                     JSONObject jo = new JSONObject(rawJsonResponse);
@@ -175,85 +220,37 @@ public class ShowQrcodeActivity extends BaseActivity {
                     JSONObject dtmplJO = new JSONObject(dtmpl);
                     JSONArray groupsJA=new JSONArray(dtmplJO.getString("groups"));
 
-                    dqphGroupsIdJO=new JSONObject();
+                    JSONObject wdddGroupsIdJO=new JSONObject();
                     for (int i=0;i<groupsJA.length();i++) {
                         JSONObject groupJO = groupsJA.getJSONObject(i);
                         String title = groupJO.getString("title");
                         String id = groupJO.getString("id");
                         Log.e("title===",""+title+",id==="+id);
-                        dqphGroupsIdJO.put(title,id);
+                        wdddGroupsIdJO.put(title,id);
                     }
-                    Log.e("dqphGroupsIdJO===",dqphGroupsIdJO.toString());
+                    Log.e("wdddGroupsIdJO===",wdddGroupsIdJO.toString());
 
-                    JSONObject dqphGroupsJO = null;
+                    JSONObject wdddGroupsJO = null;
                     for (int i=0;i<groupsJA.length();i++){
-                        if(groupsJA.getJSONObject(i).getString("id").equals(dqphGroupsIdJO.getString(ziDuanNameJO.getString("新字段组字段")))){
-                            dqphGroupsJO=groupsJA.getJSONObject(i);
-                        }
-                        else if(groupsJA.getJSONObject(i).getString("id").equals(dqphGroupsIdJO.getString(ziDuanNameJO.getString("所属订单字段")))){
-                            ssddGroupId=groupsJA.getJSONObject(i).getString("id");
-                        }
-                    }
-                    String dqphFields = dqphGroupsJO.getString("fields");
-                    Log.e("dqphFields===",dqphFields);
-                    JSONArray dqphFieldsJA = new JSONArray(dqphFields);
-
-                    dqphGroupsFieldIdJO=new JSONObject();
-                    for (int i=0;i<dqphFieldsJA.length();i++) {
-                        JSONObject dqphFieldJO = (JSONObject)dqphFieldsJA.get(i);
-                        String title = dqphFieldJO.getString("title");
-                        String id = dqphFieldJO.getString("id");
-                        Log.e("title===",""+title+",id==="+id);
-                        dqphGroupsFieldIdJO.put(title,id);
-                    }
-                    Log.e("dqphGroupsFieldIdJO===",dqphGroupsFieldIdJO.toString());
-                    getDQPHDetail();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void initSSDDJBXXFieldId(){
-        RequestParams params = AsynClient.getRequestParams();
-        AsynClient.get(MyHttpConfing.dqphDtmplRabc+ssddGroupId, this, params, new GsonHttpResponseHandler() {
-            @Override
-            protected Object parseResponse(String rawJsonData) throws Throwable {
-                return null;
-            }
-
-            @Override
-            public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
-                Log.e("ssddFail======",""+rawJsonData+","+errorResponse);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
-                Log.e("ssddSuccess======",""+rawJsonResponse);
-                try {
-                    JSONObject jo = new JSONObject(rawJsonResponse);
-                    JSONObject configJO = jo.getJSONObject("config");
-                    JSONObject dtmplJO = configJO.getJSONObject("dtmpl");
-                    JSONArray groupsJA = dtmplJO.getJSONArray("groups");
-                    JSONObject jbxxGroupJO = null;
-                    for(int i=0;i<groupsJA.length();i++){
-                        if(groupsJA.getJSONObject(i).getString("title").equals(ziDuanNameJO.getString("基本信息字段"))){
-                            jbxxGroupJO=groupsJA.getJSONObject(i);
+                        if(groupsJA.getJSONObject(i).getString("id").equals(wdddGroupsIdJO.getString(ziDuanNameJO.getString("基本信息字段")))){
+                            wdddGroupsJO=groupsJA.getJSONObject(i);
                             break;
                         }
                     }
-                    Log.e("jbxxGroupJO===",""+jbxxGroupJO);
-                    ssddJbxxFieldIdJO=new JSONObject();
-                    JSONArray jbxxFieldsJA = jbxxGroupJO.getJSONArray("fields");
-                    for(int i=0;i<jbxxFieldsJA.length();i++){
-                        JSONObject jbxxFieldsJO = jbxxFieldsJA.getJSONObject(i);
-                        String title = jbxxFieldsJO.getString("title");
-                        String id = jbxxFieldsJO.getString("id");
-                        ssddJbxxFieldIdJO.put(title,id);
+                    String wdddFields = wdddGroupsJO.getString("fields");
+                    Log.e("wdddFields===",wdddFields);
+                    JSONArray wdddFieldsJA = new JSONArray(wdddFields);
+
+                    wdddJbxxFieldIdJO=new JSONObject();
+                    for (int i=0;i<wdddFieldsJA.length();i++) {
+                        JSONObject dqphFieldJO = (JSONObject)wdddFieldsJA.get(i);
+                        String title = dqphFieldJO.getString("title");
+                        String id = dqphFieldJO.getString("id");
+                        Log.e("title===",""+title+",id==="+id);
+                        wdddJbxxFieldIdJO.put(title,id);
                     }
-                    Log.e("ssddJbxxFieldIdJO===",""+ssddJbxxFieldIdJO);
-                    getOrderDetail();
+                    Log.e("wdddJbxxFieldIdJO===",wdddJbxxFieldIdJO.toString());
+                    getWDDDJBXX();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -261,11 +258,10 @@ public class ShowQrcodeActivity extends BaseActivity {
         });
     }
 
-    private void getOrderDetail(){
+    private void getWDDDJBXX() {
         RequestParams params = AsynClient.getRequestParams();
-        Log.e("ssddCode===",""+ssddCode);
-        Log.e("ssddGroupId===",""+ssddGroupId);
-        AsynClient.get(MyHttpConfing.getDQPHDetail+ssddCode+"?fieldGroupId="+ssddGroupId, this, params, new GsonHttpResponseHandler() {
+        Log.e("wdddCode===",""+wdddCode);
+        AsynClient.get(MyHttpConfing.getWDDDDetail+wdddCode, this, params, new GsonHttpResponseHandler() {
             @Override
             protected Object parseResponse(String rawJsonData) throws Throwable {
                 return null;
@@ -273,72 +269,40 @@ public class ShowQrcodeActivity extends BaseActivity {
 
             @Override
             public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
-                Log.e("orderDetailFail======",""+rawJsonData+","+errorResponse);
+                Log.e("wdddDetailFail======",""+rawJsonData+","+errorResponse);
             }
 
             @Override
             public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
-                Log.e("ordDetSuccess======", "" + rawJsonResponse);
+                Log.e("wdddDetailSuccess======",""+rawJsonResponse);
                 try {
                     JSONObject jo = new JSONObject(rawJsonResponse);
                     JSONObject entityJO = jo.getJSONObject("entity");
                     JSONObject fieldMapJO = entityJO.getJSONObject("fieldMap");
-                    String ddh = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("订单号字段")));
-                    String yzxzl = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("预装卸重量字段")));
-                    String lxlx = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("流向类型字段")));
-                    String bjsj = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("编辑时间字段")));
-                    String sjzl = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("实际重量字段")));
-                    String zlceb = fieldMapJO.getString(ssddJbxxFieldIdJO.getString(ziDuanNameJO.getString("重量差额比字段")));
+                    String ddh = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("订单号字段")));
+                    String yzxzl = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("预装卸重量字段")));
+                    String lxlx = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("流向类型字段")));
+                    String bjsj = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("编辑时间字段")));
+                    String sjzl = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("实际重量字段")));
+                    String zlceb = fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("重量差额比字段")));
+                    String qrcodeUrl = toURLString(MyHttpConfing.baseUrl+fieldMapJO.getString(wdddJbxxFieldIdJO.getString(ziDuanNameJO.getString("二维码字段"))));
+
+                    Log.e("订单号===",""+ddh);
+                    Log.e("预装卸重量===",""+yzxzl);
+                    Log.e("流向类型===",""+lxlx);
+                    Log.e("编辑时间===",""+bjsj);
+                    Log.e("实际重量===",""+sjzl);
+                    Log.e("重量差额比===",""+zlceb);
+                    Log.e("二维码===",""+qrcodeUrl);
+
+                    initQrcode(qrcodeUrl);
+
                     ddhTV.setText(ddh);
                     yzxzlTV.setText(yzxzl);
                     lxlxTV.setText(lxlx);
                     bjsjTV.setText(bjsj);
                     sjzlTV.setText(sjzl);
                     zlcebTV.setText(zlceb);
-                    /*
-                    Log.e("ddh===",""+ddh);
-                    Log.e("yzxzl===",""+yzxzl);
-                    Log.e("lxlx===",""+lxlx);
-                    Log.e("bjsj===",""+bjsj);
-                    Log.e("sjzl===",""+sjzl);
-                    Log.e("zlceb===",""+zlceb);
-                    */
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void getDQPHDetail(){
-        RequestParams params = AsynClient.getRequestParams();
-        AsynClient.get(MyHttpConfing.getDQPHDetail+dqphCode, this, params, new GsonHttpResponseHandler() {
-            @Override
-            protected Object parseResponse(String rawJsonData) throws Throwable {
-                return null;
-            }
-
-            @Override
-            public void onFailure(int statusCode, String rawJsonData, Object errorResponse) {
-                Log.e("dqphDetailFail======",""+rawJsonData+","+errorResponse);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, String rawJsonResponse, Object response) {
-                Log.e("dqphDetailSuccess======",""+rawJsonResponse);
-                try {
-                    JSONObject jo = new JSONObject(rawJsonResponse);
-                    JSONObject entityJO = jo.getJSONObject("entity");
-                    JSONObject arrayMapJO = entityJO.getJSONObject("arrayMap");
-                    ssddCode=arrayMapJO.getJSONArray(ssddGroupId).getJSONObject(0).getString("code");
-                    Log.e("ssddCode===",""+ssddCode);
-
-                    JSONObject fieldMapJO = entityJO.getJSONObject("fieldMap");
-                    String qrcodeUrl = toURLString(MyHttpConfing.baseUrl+fieldMapJO.getString(dqphGroupsFieldIdJO.getString(ziDuanNameJO.getString("二维码字段"))));
-                    //String qrcodeUrl = toURLString("http://121.196.184.205:96/hydrocarbon/./download-files/bbe2cac353fab4605a1d9f47b4d342bf/二维码图片_司机订单.png");
-                    Log.e("qrcodeUrl===",""+qrcodeUrl);
-                    initQrcode(qrcodeUrl);
-                    initSSDDJBXXFieldId();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
